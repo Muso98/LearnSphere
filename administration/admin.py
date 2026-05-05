ef12568d3930bf2e4aa74529f3ec3926800b250d
Muso98 <musabekmusayev98@gmail.com>
2026-05-05 11:50:31 +0200

    Fix NameError in admin files and update resources admin

--- administration/admin.py
+++ administration/admin.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from modeltranslation.admin import TranslationAdmin
 from .models import Room, RoomBooking
 
--- resources/admin.py
+++ resources/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
+from .models import Resource, Quiz, Question, Answer
 
-# Register your models here.
+@admin.register(Resource)
+class ResourceAdmin(TranslationAdmin):
+    list_display = ('title', 'subject', 'resource_type')
+    list_filter = ('resource_type', 'subject')
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+@admin.register(Quiz)
+class QuizAdmin(TranslationAdmin):
+    list_display = ('title', 'subject', 'target_class', 'is_active')
+    list_filter = ('subject', 'target_class', 'is_active')
+    inlines = [QuestionInline]
+
+@admin.register(Question)
+class QuestionAdmin(TranslationAdmin):
+    list_display = ('text', 'quiz', 'points')
+
+@admin.register(Answer)
+class AnswerAdmin(TranslationAdmin):
+    list_display = ('text', 'question', 'is_correct')
